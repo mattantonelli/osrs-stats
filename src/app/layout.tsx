8459386c3b186fc87f7deb9cdb990f7902af0f26
Matt Antonelli <contact@raelys.com>
2024-01-05 12:01:36 -0500

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} d-flex flex-column`}>
         <div className="container">
           <main className="mt-3">{children}</main>
+        </div>
+        <div className="footer mt-auto py-3">
+          <div className="container text-center">
+            <span>Created by Raelys</span>
+            <a href="https://github.com/mattantonelli/osrs-stats" target="_blank" className="m-1">
+              <Image src="/images/github.png" alt="GitHub" width="25" height="25" />
+            </a>
+          </div>
         </div>
       </body>
     </html>
