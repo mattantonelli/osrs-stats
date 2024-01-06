ad06dbb71509166340820e33647f57b5a1a2d5b4
Matt Antonelli <contact@raelys.com>
2024-01-05 19:03:30 -0500

    Fix character select style

--- src/app/components/characterSelect.tsx
+++ src/app/components/characterSelect.tsx
@@ -16,7 +16,7 @@ export default function CharacterSelect({ characterList} : CharacterSelectParams
   }
 
   return (
-    <select className="form-control w-auto" onChange={(e) => selectCharacter(e.target.value)}>
+    <select className="form-select" onChange={(e) => selectCharacter(e.target.value)}>
       <option value="">Select a character</option>
       {characterList.map((character) => {
         return <option value={character} key={character}>{character}</option>;
